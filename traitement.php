4683f06137419e559da8741e0789c27473139cd0
Casperdev4 <allam.bilal91@gmail.com>
2024-01-14 17:39:36 +0100

    hh

--- traitement.php
+++ traitement.php
@@ -18,8 +18,6 @@ $message .= "Date: $date \n";
 $message .= "Heure: $heure \n";
 $message .= "Passagers: $passagers \n";
 
-//Import PHPMailer classes into the global namespace
-//These must be at the top of your script, not inside a function
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
@@ -28,25 +26,22 @@ require 'PHPMailer/src/Exception.php';
 require 'PHPMailer/src/PHPMailer.php';
 require 'PHPMailer/src/SMTP.php';
 
-//Create an instance; passing `true` enables exceptions
 $mail = new PHPMailer(true);
 
 try {
-    //Server settings
-    $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.ionos.fr';                     //Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'contact@webprime.fr';                     //SMTP username
-    $mail->Password   = 'Allamalyjass912!';                               //SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    $mail->isSMTP();                      
+    $mail->Host       = 'smtp.ionos.fr';        
+    $mail->SMTPAuth   = true;                
+    $mail->Username   = 'contact@webprime.fr';
+    $mail->Password   = 'Allamalyjass912!';     
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;       
+    $mail->Port       = 465; 
 
-    //Recipients
     $mail->setFrom('contact@webprime.fr', 'WebPrime');
-    $mail->addAddress('allam.bilal91@gmail.com');     //Add a recipient
+    $mail->addAddress('allam.bilal91@gmail.com'); 
+    $mail->addAddress('contact@webprime.fr'); 
 
-    //Content
-    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->isHTML(true);   
     $mail->Subject = 'Formulaire de contact';
     $mail->Body    = $message;
     $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
