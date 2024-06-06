25cc35408f398b061edbe61e63ebfb5a78d576c4
Casperdev4 <allam.bilal91@gmail.com>
2024-06-06 18:37:35 +0200

    fgh

--- traitement.php
+++ traitement.php
@@ -2,6 +2,12 @@
 
 header('Content-Type: text/html; charset=UTF-8');
 
+function contains_links_or_scripts($text) {
+    $linkPattern = "/https?:\/\/[^\s]+|<a\s+href\s*=\s*['\"]?[^\s>]+['\"]?/i";
+    $scriptPattern = "/<script\b[^>]*>(.*?)<\/script>/is";
+    return preg_match($linkPattern, $text) || preg_match($scriptPattern, $text);
+}
+
 $nom = htmlspecialchars($_POST['nom'], ENT_QUOTES, 'UTF-8');
 $telephone = htmlspecialchars($_POST['telephone'], ENT_QUOTES, 'UTF-8');
 $depart = htmlspecialchars($_POST['lieu_depart'], ENT_QUOTES, 'UTF-8');
@@ -9,12 +15,17 @@ $arrive = htmlspecialchars($_POST['lieu_arrivee'], ENT_QUOTES, 'UTF-8');
 $passagers = htmlspecialchars($_POST['passagers'], ENT_QUOTES, 'UTF-8');
 $bagages = htmlspecialchars($_POST['bagages'], ENT_QUOTES, 'UTF-8');
 
+if (contains_links_or_scripts($depart) || contains_links_or_scripts($arrive)) {
+    echo "Pas autorisés.";
+    exit();
+}
+
 $message = "NOM : $nom \n";
-$message .= "/ TÉLÉPHONE : $telephone \n";
-$message .= "/ DÉPART : $depart \n";
-$message .= "/ ARRIVÉE : $arrive \n";
-$message .= "/ PASSAGERS : $passagers \n";
-$message .= "/ BAGAGES : $bagages \n";
+$message .= "TÉLÉPHONE : $telephone \n";
+$message .= "DÉPART : $depart \n";
+$message .= "ARRIVÉE : $arrive \n";
+$message .= "PASSAGERS : $passagers \n";
+$message .= "BAGAGES : $bagages \n";
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
@@ -27,7 +38,6 @@ require 'PHPMailer/src/SMTP.php';
 $mail = new PHPMailer(true);
 
 try {
-
     $mail->isSMTP();
     $mail->Host       = 'smtp.ionos.fr';
     $mail->SMTPAuth   = true;
@@ -35,7 +45,6 @@ try {
     $mail->Password   = 'Allamalyjass912!';
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
     $mail->Port       = 465;
-
 
     $mail->setFrom('contact@webprime.fr', 'ASC-DRIVER');
     $mail->addAddress('asc.driver@outlook.com');
@@ -55,4 +64,3 @@ try {
     echo "Message non envoyé. Mailer Error: {$mail->ErrorInfo}";
 }
 ?>
-
