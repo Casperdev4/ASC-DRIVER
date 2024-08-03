f9651d3592afccbc02add039d54f8f9a8df5726a
Casperdev4 <allam.bilal91@gmail.com>
2024-08-03 17:26:42 +0200

    fg

--- traitement.php
+++ traitement.php
@@ -5,10 +5,21 @@ header('Content-Type: text/html; charset=UTF-8');
 function sanitize_input($data) {
     $data = strip_tags($data);
     $data = htmlspecialchars($data, ENT_QUOTES, 'UTF-8');
-    $data = preg_replace('/<a\s+href\s*=\s*["\'][^"\']*["\']/i', '[Lien supprimé]', $data);
-    $data = preg_replace('/<script\b[^>]*>([\s\S]*?)<\/script>/i', '[Script supprimé]', $data);
-    $data = preg_replace('/https?:\/\/[^\s]+/i', '[URL supprimée]', $data);
     return $data;
+}
+
+function contient_liens($texte) {
+    $patternLien = "/https?:\/\/[^\s]+|<a\s+href\s*=\s*['\"]?[^\s>]+['\"]?/i";
+    $patternScript = "/<script[^>]*>[\s\S]*?<\/script>/i";
+    return preg_match($patternLien, $texte) || preg_match($patternScript, $texte);
+}
+
+function est_vide($champ) {
+    return !isset($champ) || trim($champ) === '';
+}
+
+function contient_cyrillique($texte) {
+    return preg_match("/[\p{Cyrillic}]/u", $texte);
 }
 
 $nom = sanitize_input($_POST['nom']);
@@ -24,6 +35,23 @@ $bagages = sanitize_input($_POST['bagages']);
 $sieges_auto = sanitize_input($_POST['sieges_auto']);
 $commentaires = sanitize_input($_POST['commentaires']);
 
+// Validation
+if (est_vide($nom) || est_vide($telephone) || est_vide($depart) || est_vide($arrive) || est_vide($date) || est_vide($heure)) {
+    echo "Certains champs obligatoires sont manquants.";
+    exit();
+}
+
+if (contient_liens($commentaires)) {
+    echo "Le formulaire contient des liens ou des scripts interdits.";
+    exit();
+}
+
+if (contient_cyrillique($commentaires)) {
+    echo "Les caractères cyrilliques ne sont pas autorisés.";
+    exit();
+}
+
+// Préparation du message
 $message = "NOM : $nom\n";
 $message .= "TÉLÉPHONE : $telephone\n";
 $message .= "DÉPART : $depart\n";
@@ -37,6 +65,7 @@ $message .= "BAGAGES : $bagages\n";
 $message .= "SIÈGES AUTO : $sieges_auto\n";
 $message .= "COMMENTAIRES : $commentaires\n";
 
+// Envoi de l'email
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
@@ -74,6 +103,4 @@ try {
     echo "Message non envoyé. Mailer Error: {$mail->ErrorInfo}";
 }
 ?>
-
-
 
